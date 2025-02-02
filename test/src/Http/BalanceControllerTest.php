8aac5d2ea1f975aa8c109718a0520b08d13f4f51
Rafael Garcia de Carvalho <rafaelgarcia95@gmail.com>
2025-02-01 22:55:40 -0300

    Update balance controller to use AccountService (#6)

--- test/src/Http/BalanceControllerTest.php
+++ test/src/Http/BalanceControllerTest.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 
 namespace Test\Http;
 
+use App\Services\AccountService\Repository\Account;
+use App\Services\AccountService\Repository\AccountRepository;
 use Test\CustomTestCase;
+use Test\Support\Services\AccountService\Repository\AccountRepositoryForTests;
 
 class BalanceControllerTest extends CustomTestCase {
 
+	/** @before */
+	public function resetAccountRepository(): void {
+		AccountRepositoryForTests::reset();
+	}
+	
 	public function testController_WhenSuccessful(): void {
+		$this->simulateAccountWithAmount('1', '10');
+		
 		$result = $this->request_simulator
 			->withMethod('GET')
 			->withBody([])
@@ -17,7 +27,24 @@ class BalanceControllerTest extends CustomTestCase {
 			->dispatch();
 
 		$this->assertSame(200, $result->getStatusCode());
-		$this->assertSame('Balance 1', (string) $result->getBody());
+		$this->assertJson('{"amount": "10"}', (string) $result->getBody());
+	}
+
+	public function testController_WhenAccountDoesNotExist(): void {
+		$result = $this->request_simulator
+			->withMethod('GET')
+			->withBody([])
+			->withPath('/balance')
+			->withQueryParam(name: 'id', value: '1')
+			->dispatch();
+
+			$this->assertSame(404, $result->getStatusCode());
+	}
+
+	private function simulateAccountWithAmount(string $id, string $amount): void {
+		$account = new Account($id, $amount);
+		$account_repository = new AccountRepository();
+		$account_repository->createAccount($account);
 	}
 
 }
